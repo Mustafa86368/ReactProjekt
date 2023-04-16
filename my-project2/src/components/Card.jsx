52ba80ee6c377cbefc3c153b1389d18292e1b1c9
Mustafa86368 <mustafa.budak@gmx.net>
2023-04-17 01:12:27 +0200

    add tags

--- my-project2/src/components/Card.jsx
+++ my-project2/src/components/Card.jsx
@@ -5,8 +5,15 @@ const Card = () => {
     <div className='card'>
         <img src="./src/assets/kahve.png" alt="" />
         <div className='card--stats'>
-            
+            <img src="./src/assets/star.png" alt="" />
+            <span>5.0</span>
+            <span> (6) .</span>
+            <span>USA</span>
 
+        </div>
+        <div>
+        <p>Life Lessons with Katie Zaferes</p>
+            <p>From $136</p>
         </div>
     </div>
   )
