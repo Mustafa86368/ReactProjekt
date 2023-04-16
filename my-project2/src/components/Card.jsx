9dd630648eea966b3cee22309e8e49f1c07a66fc
Mustafa86368 <mustafa.budak@gmx.net>
2023-04-17 01:27:43 +0200

    add tag and styling

--- my-project2/src/components/Card.jsx
+++ my-project2/src/components/Card.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 const Card = () => {
   return (
     <div className='card'>
-        <img src="./src/assets/kahve.png" alt="" />
+        <img src="./src/assets/kahve.png" alt="" className='card--image'/>
         <div className='card--stats'>
-            <img src="./src/assets/star.png" alt="" />
+            <img src="./src/assets/star.png" alt="" className='card--star'/>
             <span>5.0</span>
-            <span> (6) .</span>
-            <span>USA</span>
+            <span className='gray'> (6) .</span>
+            <span className='gray'>USA</span>
 
         </div>
         <div>
         <p>Life Lessons with Katie Zaferes</p>
-            <p>From $136</p>
+            <p><span className='bold'>From $136</span> / person</p>
         </div>
     </div>
   )
