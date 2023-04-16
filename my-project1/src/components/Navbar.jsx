4ed481170c3710e07d6206303d93714702adbf02
Mustafa86368 <mustafa.budak@gmx.net>
2023-04-16 15:18:38 +0200

    componentler olusturuldu

--- my-project1/src/components/Navbar.jsx
+++ my-project1/src/components/Navbar.jsx
@@ -4,9 +4,9 @@ export const Navbar = () => {
   return (
    <div>
    <nav>
-    <img src="./src/assets/react.svg" alt="" />
-    <h3>ReactsFacts</h3>
-    <h4>React Course - Project</h4>
+    <img src="./src/assets/react.svg" alt="" className='nav--icon' />
+    <h3 className='nav--logo_text'>ReactsFacts</h3>
+    <h4 className='nav--title'>React Course - Project</h4>
    </nav>
    </div>
   )
