3adc2b2869e8bf72671fa795b174bb7d428a1794
Mustafa86368 <mustafa.budak@gmx.net>
2023-04-16 22:15:19 +0200

    styl section

--- my-project2/src/components/Hero.jsx
+++ my-project2/src/components/Hero.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-        <section>
-            <img src="./src/assets/photo-grid.png" alt="" />
-            <h1>Online Experiences</h1>
-            <p>Join unique interactive activities led by 
+        <section className='hero'>
+            <img src="./src/assets/photo-grid.png" alt="" className='hero--photo' />
+            <h1 className='hero--header'>Online Experiences</h1>
+            <p className='hero--text'>Join unique interactive activities led by 
             one-of-a-kind hosts—all without leaving home.</p>
         </section>
     </div>
