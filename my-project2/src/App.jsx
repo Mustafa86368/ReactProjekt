fca7292ee8a0d1df1e7783bad9f7ba02948da257
Mustafa86368 <mustafa.budak@gmx.net>
2023-04-20 00:23:13 +0200

    add props

--- my-project2/src/App.jsx
+++ my-project2/src/App.jsx
@@ -9,16 +9,20 @@ import './App.css'
 console.log(data)
 function App() {
   const cards = data.map(item=> {
-    return <Card img = {item.coverImg} 
+    return (
+    <Card
+    key = {item.id} 
+    img = {item.coverImg} 
     rating = {item.stats.rating}
     reviewCount = {item.stats.reviewCount}
     location = {item.location}
     title = {item.title}
     price = {item.price} 
     openSpots = {item.openSpots}
-   
+  
 
     />
+    )
   })
 
   return (
