dccfbaef5437325c9dc6400a70800063b1b100f3
Mustafa86368 <mustafa.budak@gmx.net>
2023-04-20 14:05:18 +0200

    style header

--- my-project3/src/components/Header.jsx
+++ my-project3/src/components/Header.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export const Header = () => {
   return (
   <header>
-    
+    <img src="./public/images/troll-face.png" alt="" className='header--icon' />
+    <h3 className='head--logo_text'>Meme Genarator</h3>
+    <h4 className='header--title'>React Course - Project 3</h4>
   </header>
   )
 }
