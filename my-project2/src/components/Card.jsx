2d45753a61494b4fdb5ab40a073627438cdac70c
Mustafa86368 <mustafa.budak@gmx.net>
2023-04-20 12:50:36 +0200

    add infos

--- my-project2/src/components/Card.jsx
+++ my-project2/src/components/Card.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 
 const Card = (props) => {
   let badgeText
-  if (props.item.openSpots === 0) {
+  if (props.openSpots === 0) {
     badgeText = "SOLD OUT"
-  } else if (props.item.location === "Online") {
+  } else if (props.location === "Online") {
     badgeText = "ONLINE"
   }
   return (
     <div className='card'>
       {badgeText && <div className="card--badge">{badgeText}</div>}
-        <img src={props.item.coverImg} alt="" className='card--image'/>
+        <img src={props.coverImg} alt="" className='card--image'/>
         <div className='card--stats'>
             <img src="./public/images/star.png" alt="" className='card--star'/>
-            <span>{props.item.stats.rating}</span>
-            <span className='gray'>  ({props.item.stats.reviewCount}).</span>
-            <span className='gray'>{props.item.location}</span>
+            <span>{props.stats.rating}</span>
+            <span className='gray'>  ({props.stats.reviewCount}).</span>
+            <span className='gray'>{props.location}</span>
 
         </div>
         <div>
-        <p>{props.item.title}</p>
-            <p><span className='bold'>From ${props.item.price}</span> / person</p>
+        <p>{props.title}</p>
+            <p><span className='bold'>From ${props.price}</span> / person</p>
         </div>
     </div>
   )
